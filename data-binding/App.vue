8cd5ceae1835d76366ec08fa36978d1ba0c4d6a1
DouglasPimentel <dev.pimentel@gmail.com>
2021-01-20 20:10:34 -0300

    Data binding

---
+++ data-binding/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1 :style="{ textDecoration: decoration }">Hello {{ name }}</h1>
+    <input type="text" v-model="name" />
+    <br />
+    <a :href="link">Link para site do Vue.js</a>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    name: 'Vue.js Brasil',
+    link: 'https://vuejs.org/',
+    decoration: 'underline'
+  })
+}
+</script>
