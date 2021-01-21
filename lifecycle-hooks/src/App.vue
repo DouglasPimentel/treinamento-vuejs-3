cab90434a0678dca05c2b18e09ee6cd329c8c39f
DouglasPimentel <dev.pimentel@gmail.com>
2021-01-20 21:04:37 -0300

    Life cycle hooks

---
+++ lifecycle-hooks/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Life Cycle Hooks</h1>
+  </div>
+</template>
+
+<script>
+import { onMounted } from 'vue'
+
+export default {
+  setup() {
+    onMounted(() => {
+      console.log('Foi montado com composition API')
+    })
+  }
+}
+</script>
